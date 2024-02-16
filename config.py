82eaa2531e46043d64fd47eb6b1a87884831b198
Diwanabot <156938668+Diwanabot@users.noreply.github.com>
2024-02-16 17:21:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Godfatherakkii/TGN-MUSIC",
+    "https://telegra.ph/file/b43b940d074116bce9d09.jpg",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Haal_e_Dil_ll")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/neeraj_world")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/raja_luck_vip1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/raja_luck_vip1")
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
@@ -83,13 +83,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/08cfa0e2f267e8e421f80.png"
+    "START_IMG_URL", "https://telegra.ph/file/b43b940d074116bce9d09.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/08cfa0e2f267e8e421f80.png"
+    "PING_IMG_URL", "https://telegra.ph/file/b43b940d074116bce9d09.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL =    "https://telegra.ph/file/08cfa0e2f267e8e421f80.png"
+STATS_IMG_URL =    "https://telegra.ph/file/b43b940d074116bce9d09.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
