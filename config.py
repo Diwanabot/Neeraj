3fa024c4abf6ecab068fe11566247cf88f5bcf48
Diwanabot <156938668+Diwanabot@users.noreply.github.com>
2024-02-16 17:29:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://telegra.ph/file/b43b940d074116bce9d09.jpg",
+    "https://github.com/Diwanabot/Neeraj",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
